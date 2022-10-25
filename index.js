5424222051f7ce369b037e3ec516ad793aa7f24b
Atik2788 <ashanto653.2@gmail.com>
2022-10-26 00:17:41 +0600

    after add some card details

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors())
 
 const port = process.env.PORT || 5000;
 
@@ -11,6 +14,12 @@ app.get('/', (req, res) =>{
 
 app.get('/courses', (req, res) =>{
     res.send(courses)
+})
+
+app.get('/courses-details/:id', (req, res) => {
+    const id = req.params.id;
+    const courses_id = courses.find(n => n.id === parseInt(id))
+    res.send(courses_id)
 })
 
 
